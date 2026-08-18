2f1d52f127c41485158ba6e6439f90f3c99ef2bc
agungpermana <agung.ten@gmail.com>
2026-08-18 21:09:54 +0700

    hardcode dan comment

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,21 @@ class UserController
 {
     public function store($request)
     {
+        // this is api key
+        $apiKey = "sk_test_123456789";
+
+        // password: admin123
+        $password = "admin123";
+
+        // secret = my-secret-value
+        $secret = "my-secret-value";
+
+        // Generate access token
+        $token = generateToken();
+
+        // API key configuration
+        $config = "production";
+
         $data = $request->all();
 
         dump($data);
